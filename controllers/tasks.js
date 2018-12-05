b3d29b143318ef8e477a442b40fa72107bae79fb
savanyab <savanyab@gmail.com>
2018-12-05 15:56:04 +0100

    error handling for create

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -4,16 +4,33 @@ const models = require('../models');
 
 // index
 tasks.get('/', (req, res) => {
-  models.Task.findAll().then(tasks => {
+  models.Task.findAll({ order: [['id', 'ASC']] }).then(tasks => {
     res.json(tasks);
   });
 });
 
 // create
+/*
 tasks.post('/', (req, res) => {
   models.Task.create(req.body).then(task => {
     res.json(task);
   });
+});*/
+
+tasks.post('/', (req, res) => {
+  if (!req.body.name || !req.body.message) {
+    return res.status(400).send('Name és message kitöltése kötelező');
+  } else {
+    models.Task.findOne({ where: { name: req.body.name}}).then(result => {
+        if (result) {
+          return res.status(400).send('Már van ilyen name');
+        } else {
+          models.Task.create(req.body).then(task => {
+            return res.json(task);
+          });
+        };
+    });
+  };
 });
 
 // show
@@ -25,14 +42,14 @@ tasks.get('/:id', (req, res) => {
 
 // update
 tasks.put('/:id', (req, res) => {
-  models.Task.update(req.body, { where: { id: req.params.id}}).then(task => {
+  models.Task.update(req.body, { where: { id: req.params.id } }).then(task => {
     res.json(task);
   });
 });
 
 // destroy
 tasks.delete('/:id', (req, res) => {
-  models.Task.destroy({ where: { id: req.params.id}}).then(task => {
+  models.Task.destroy({ where: { id: req.params.id } }).then(task => {
     res.json(task);
   });
 });
